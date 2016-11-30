f7f65c0acf1223eaafd0fab6a81b6ae31469ca4b
Cognitive <alok.g@cognitiveclouds.com>
2016-11-30 12:50:44 +0530

    Removed unused class

--- app/src/main/java/com/sortedqueue/programmercreek/activity/DashboardActivity.java
+++ app/src/main/java/com/sortedqueue/programmercreek/activity/DashboardActivity.java
@@ -145,23 +145,6 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
         finish();
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_dashboard,
-                    container, false);
-            return rootView;
-        }
-    }
-
     @Override
     public void onClick(View v) {
 
