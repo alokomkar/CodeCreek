a00af1121d350c13217bcb20ed462f44dbe9e9e4
Alok Omkar <alokomkar.gudikote@gmail.com>
2017-01-02 13:31:49 +0530

    Custom animations

--- app/src/main/java/com/sortedqueue/programmercreek/adapter/CustomProgramRecyclerViewAdapter.java
+++ app/src/main/java/com/sortedqueue/programmercreek/adapter/CustomProgramRecyclerViewAdapter.java
@@ -6,6 +6,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -28,6 +30,10 @@ public class CustomProgramRecyclerViewAdapter extends RecyclerView.Adapter<Custo
     private List<Program_Index> mProgram_Indexs;
     private AdapterClickListner mAdapterClickListner;
 
+    private int lastPosition = -1;
+    private Animation bottomUpAnimation;
+    private Animation topDownAnimation;
+
     public interface AdapterClickListner {
         void onItemClick( int position );
     }
@@ -37,6 +43,8 @@ public class CustomProgramRecyclerViewAdapter extends RecyclerView.Adapter<Custo
         this.mProgram_Indexs = mProgram_indexs;
         this.mAdapterClickListner = (AdapterClickListner) context;
         mProgramType = new CreekPreferences(mContext).getProgramLanguage().substring(0, 1).toUpperCase();
+        bottomUpAnimation = AnimationUtils.loadAnimation(mContext, R.anim.item_up_from_bottom);
+        topDownAnimation = AnimationUtils.loadAnimation(mContext, R.anim.item_up_from_bottom);
     }
 
     @Override
@@ -49,6 +57,8 @@ public class CustomProgramRecyclerViewAdapter extends RecyclerView.Adapter<Custo
     public void onBindViewHolder(ViewHolder holder, int position) {
         holder.programTypeTextView.setText(mProgramType);
         holder.txtViewProgDescription.setText(mProgram_Indexs.get(position).getProgram_Description());
+        //holder.itemView.startAnimation((position > lastPosition) ? bottomUpAnimation : topDownAnimation );
+        //lastPosition = position;
     }
 
     @Override
