06901ecc3976e436338d1119b67d81b4d5cf87ca
Alok Omkar <alokomkar.gudikote@gmail.com>
2017-01-30 11:49:20 +0530

    UI fix, added intro for sql

--- app/src/main/java/com/sortedqueue/programmercreek/activity/IntroActivity.java
+++ app/src/main/java/com/sortedqueue/programmercreek/activity/IntroActivity.java
@@ -121,6 +121,8 @@ public class IntroActivity extends AppCompatActivity
     private void loadChapters() {
         introChapters = new ArrayList<>();
         programLanguage = new CreekPreferences(IntroActivity.this).getProgramLanguage();
+        /*programLanguage = "sql";
+        new CreekPreferences(IntroActivity.this).setProgramLanguage(programLanguage);
         IntroChapter introChapter = new IntroChapter();
         introChapter.setChapterHeader("Introduction");
         introChapter.setChapterId("sql_intro_1");
@@ -157,86 +159,155 @@ public class IntroActivity extends AppCompatActivity
         introChapters.add(introChapter);
 
         introChapter = new IntroChapter();
-        introChapter.setChapterHeader("Basics with your first program");
+        introChapter.setChapterHeader("What is RDBMS?");
         introChapter.setChapterId("sql_intro_2");
         introChapter.setChapterIndex(2);
-        introChapter.setChapterIntro("The POSIX Environment\n" +
-                "Although POSIX was developed on UNIX, a POSIX complaint system is not necessarily a UNIX system. A few UNIX conventions have different meanings according to the POSIX standards. Most C and C++ header files are stored under the /usr/include directory in any UNIX system and each of them is referenced by #include<header-file-name> This method is adopted in POSIX. There need not be a physical file of that name existing on a POSIX conforming system.\n" +
-                "The POSIX Feature Test Macros\n" +
-                "POSIX.1 defines a set of feature test macro’s which if defined on a system, means that the system has implemented the corresponding features.");
-        introChapter.setChapterNote(
-                "_POSIX_JOB_CONTROL : The system supports the BSD style job control.\n" +
-                "_POSIX_SAVED_IDS :" +
-                "Each process running on the system keeps the saved set UID and the set-GID, so that they can change its effective user-ID and group-ID to those values via seteuid and setegid API's. _POSIX_CHOWN_RESTRICTED If the defined value is -1, users may change ownership of files owned by them, otherwise only users with special privilege may change ownership of any file on the system.\n" +
-                "_POSIX_NO_TRUNC :" +
-                "If the defined value is -1, any long pathname passed to an API is silently truncated to NAME_MAX bytes, otherwise error is generated.\n " +
-                        "_POSIX_VDISABLE : If defined value is -1, there is no disabling character for special characters for all terminal device files. Otherwise the value is the disabling character value.");
-        introChapter.setChapterProgram(
-                "The following test_config.C illustrates the use of sysconf, pathcong and fpathconf:\n " +
-                        "#define _POSIX_SOURCE\n" +
-                        "#define _POSIX_C_SOURCE 199309L\n " +
-                        "#include<stdio.h>\n " +
-                        "#include<iostream.h>\n " +
-                        "#include<unistd.h>\n " +
-                        "int main() {\n " +
-                        "int res;\n " +
-                        "if((res=sysconf(_SC_OPEN_MAX))==-1)\n " +
-                        "perror(“sysconf”);\n " +
-                        "else\n" +
-                        "cout<<”OPEN_MAX:”<<res<<endl;\n" +
-                        "if((res=pathconf(“/”,_PC_PATH_MAX))==-1)\n " +
-                        "perror(“pathconf”);\n " +
-                        "else cout<<”max path name:”<<(res+1)<<endl;\n " +
-                        "if((res=fpathconf(0,_PC_CHOWN_RESTRICTED))==-1)\n " +
-                        "perror(“fpathconf”);\n " +
-                        "else\n " +
-                        "cout<<”chown_restricted for stdin:”<<res<<endl;\n " +
-                        "return 0;\n " +
-                        "}");
-
+        introChapter.setChapterIntro("RDBMS stands for Relational Database Management System. " +
+                "RDBMS is the basis for SQL, and for all modern database systems like MS SQL Server, IBM DB2, Oracle, MySQL, and Microsoft Access.\n" +
+                "\n" +
+                "A Relational database management system (RDBMS) is a database management system (DBMS) that is based on the relational model as introduced by E. F. Codd.\n\n" +
+                "What is table?\n" +
+                "The data in RDBMS is stored in database objects called tables. The table is a collection of related data entries and it consists of columns and rows.\n\n" +
+                "Following is the example of a CUSTOMERS table:\n" +
+                "\n" +
+                "+----+----------+-----+-----------+----------+\n" +
+                "| ID | NAME     | AGE | ADDRESS   | SALARY   |\n" +
+                "+----+----------+-----+-----------+----------+\n" +
+                "|  1 | Ramesh   |  32 | Ahmedabad |  2000.00 |\n" +
+                "|  2 | Khilan   |  25 | Delhi     |  1500.00 |\n" +
+                "|  3 | kaushik  |  23 | Kota      |  2000.00 |\n" +
+                "|  4 | Chaitali |  25 | Mumbai    |  6500.00 |\n" +
+                "|  5 | Hardik   |  27 | Bhopal    |  8500.00 |\n" +
+                "|  6 | Komal    |  22 | MP        |  4500.00 |\n" +
+                "|  7 | Muffy    |  24 | Indore    | 10000.00 |\n" +
+                "+----+----------+-----+-----------+----------+\n\n" +
+                "What is field?\n" +
+                "Every table is broken up into smaller entities called fields. The fields in the CUSTOMERS table consist of ID, NAME, AGE, ADDRESS and SALARY.\n" +
+                "\n" +
+                "A field is a column in a table that is designed to maintain specific information about every record in the table.\n" +
+                "\n" +
+                "What is record or row?\n" +
+                "A record, also called a row of data, is each individual entry that exists in a table. For example there are 7 records in the above CUSTOMERS table. Following is a single row of data or record in the CUSTOMERS table:\n" +
+                "\n" +
+                "+----+----------+-----+-----------+----------+\n" +
+                "|  1 | Ramesh   |  32 | Ahmedabad |  2000.00 |\n" +
+                "+----+----------+-----+-----------+----------+\n" +
+                "A record is a horizontal entity in a table.\n" +
+                "\n" +
+                "What is column?\n" +
+                "A column is a vertical entity in a table that contains all information associated with a specific field in a table.\n" +
+                "\n" +
+                "For example, a column in the CUSTOMERS table is ADDRESS, which represents location description and would consist of the following:\n" +
+                "\n" +
+                "+-----------+\n" +
+                "| ADDRESS   |\n" +
+                "+-----------+\n" +
+                "| Ahmedabad |\n" +
+                "| Delhi     |\n" +
+                "| Kota      |\n" +
+                "| Mumbai    |\n" +
+                "| Bhopal    |\n" +
+                "| MP        |\n" +
+                "| Indore    |\n" +
+                "+----+------+\n" +
+                "What is NULL value?\n" +
+                "A NULL value in a table is a value in a field that appears to be blank, which means a field with a NULL value is a field with no value.\n" +
+                "\n" +
+                "It is very important to understand that a NULL value is different than a zero value or a field that contains spaces. A field with a NULL value is one that has been left blank during record creation.");
         introChapter.setChapterLanguage(programLanguage);
 
         introChapters.add(introChapter);
 
         introChapter = new IntroChapter();
-        introChapter.setChapterHeader("Using APIs");
+        introChapter.setChapterHeader("Extras");
         introChapter.setChapterId("sql_intro_3");
         introChapter.setChapterIndex(3);
-        introChapter.setChapterIntro("UNIX AND POSIX APIs\n" +
-                "API \uF0E0 A set of application programming interface functions that can be called by user programs to perform system specific functions. Most UNIX systems provide a common set of API’s to perform the following functions.\n" +
-                "\uF0A7 Determine the system configuration and user information.\n" +
-                "\uF0A7 Files manipulation.\n" +
-                "\uF0A7 Processes creation and control.\n" +
-                "\uF0A7 Inter-process communication.\n" +
-                "\uF0A7 Signals and daemons\n" +
-                "\uF0A7 Network communication.\n" +
-                "The POSIX APIs\n" +
-                "In general POSIX API’s uses and behaviours’ are similar to those of Unix API’s. However, user’s programs should define the _POSIX_SOURCE or _POSIX_C_SOURCE in their programs to enable the POSIX API’s declaration in header files that they include.");
-        introChapter.setChapterNote("API Common Characteristics\n" +
-                "\uF0A7 Many APIs returns an integer value which indicates the termination status of their execution\n" +
-                "\uF0A7 API return -1 to indicate the execution has failed, and the global variable errno is set with an error code.\n" +
-                "\uF0A7 a user process may call perror function to print a diagnostic message of the failure to the std o/p, or it may call strerror function and gives it errno as the actual argument value; the strerror function returns a diagnostic message string and the user process may print that message in its preferred way\n" +
-                "\uF0A7 the possible error status codes that may be assigned to errno by any API are defined in the <errno.h> header.");
+        introChapter.setChapterIntro("SQL Constraints:\n" +
+                "Constraints are the rules enforced on data columns on table. These are used to limit the type of data that can go into a table. This ensures the accuracy and reliability of the data in the database.\n" +
+                "\n" +
+                "Constraints could be column level or table level. Column level constraints are applied only to one column where as table level constraints are applied to the whole table.\n" +
+                "\n" +
+                "Following are commonly used constraints available in SQL:\n" +
+                "\n" +
+                "NOT NULL Constraint: Ensures that a column cannot have NULL value.\n" +
+                "\n" +
+                "DEFAULT Constraint: Provides a default value for a column when none is specified.\n" +
+                "\n" +
+                "UNIQUE Constraint: Ensures that all values in a column are different.\n" +
+                "\n" +
+                "PRIMARY Key: Uniquely identified each rows/records in a database table.\n" +
+                "\n" +
+                "FOREIGN Key: Uniquely identified a rows/records in any another database table.\n" +
+                "\n" +
+                "CHECK Constraint: The CHECK constraint ensures that all values in a column satisfy certain conditions.\n" +
+                "\n" +
+                "INDEX: Use to create and retrieve data from the database very quickly.\n" +
+                "\n" +
+                "Data Integrity:\n" +
+                "The following categories of the data integrity exist with each RDBMS:\n" +
+                "\n" +
+                "Entity Integrity: There are no duplicate rows in a table.\n" +
+                "\n" +
+                "Domain Integrity: Enforces valid entries for a given column by restricting the type, the format, or the range of values.\n" +
+                "\n" +
+                "Referential integrity: Rows cannot be deleted, which are used by other records.\n" +
+                "\n" +
+                "User-Defined Integrity: Enforces some specific business rules that do not fall into entity, domain or referential integrity.\n" +
+                "\n");
 
         introChapter.setChapterLanguage(programLanguage);
 
         introChapters.add(introChapter);
 
         introChapter = new IntroChapter();
-        introChapter.setChapterHeader("The UNIX and POSIX Development Environment");
+        introChapter.setChapterHeader("Databases");
         introChapter.setChapterId("sql_intro_4");
         introChapter.setChapterIndex(4);
-        introChapter.setChapterIntro("The UNIX and POSIX Development Environment\n" +
-                "POSIX provides portability at the source level. This means that you transport your source program to the target machine, compile it with the standard C compiler using conforming headers and link it with the standard libraries." +
-                "\nSome commonly used POSIX.1 and UNIX API’s are declared in <unistd.h> header. Most of POSIX.1, POSIX>1b and UNIX API object code is stored in the libc.a and lib.so libraries.");
+        introChapter.setChapterIntro("SQL - RDBMS Databases\n" +
+                "There are many popular RDBMS available to work with. This tutorial gives a brief overview of few most popular RDBMS. This would help you to compare their basic features.\n" +
+                "\n" +
+                "MySQL\n" +
+                "MySQL is an open source SQL database, which is developed by Swedish company MySQL AB. MySQL is pronounced \"my ess-que-ell,\" in contrast with SQL, pronounced \"sequel.\"\n" +
+                "\n" +
+                "MySQL is supporting many different platforms including Microsoft Windows, the major Linux distributions, UNIX, and Mac OS X.\n" +
+                "\n" +
+                "MySQL has free and paid versions, depending on its usage (non-commercial/commercial) and features. MySQL comes with a very fast, multi-threaded, multi-user, and robust SQL database server.\n\n" +
+                "MS SQL Server\n" +
+                "MS SQL Server is a Relational Database Management System developed by Microsoft Inc. Its primary query languages are:\n" +
+                "\n" +
+                "T-SQL.\n" +
+                "\n" +
+                "ANSI SQL.\n\n" +
+                "ORACLE\n" +
+                        "It is a very large and multi-user database management system. Oracle is a relational database management system developed by 'Oracle Corporation'.\n" +
+                        "\n" +
+                        "Oracle works to efficiently manage its resource, a database of information, among the multiple clients requesting and sending data in the network.\n" +
+                        "\n" +
+                        "It is an excellent database server choice for client/server computing. Oracle supports all major operating systems for both clients and servers, including MSDOS, NetWare, UnixWare, OS/2 and most UNIX flavors.\n\n" +
+                "MS ACCESS\n" +
+                        "This is one of the most popular Microsoft products. Microsoft Access is an entry-level database management software. MS Access database is not only an inexpensive but also powerful database for small-scale projects.\n" +
+                        "\n" +
+                        "MS Access uses the Jet database engine, which utilizes a specific SQL language dialect (sometimes referred to as Jet SQL).\n" +
+                        "\n" +
+                        "MS Access comes with the professional edition of MS Office package. MS Access has easy-to-use intuitive graphical interface.\n" +
+                        "\n" +
+                        "1992 - Access version 1.0 was released.\n" +
+                        "\n" +
+                        "1993 - Access 1.1 released to improve compatibility with inclusion the Access Basic programming language.\n" +
+                        "\n" +
+                        "The most significant transition was from Access 97 to Access 2000\n" +
+                        "\n" +
+                        "2007 - Access 2007, a new database format was introduced ACCDB which supports complex data types such as multi valued and attachment fields.\n" +
+                        "\n"
+                );
         introChapter.setChapterLanguage(programLanguage);
 
         introChapters.add(introChapter);
 
-        initChapter( introChapters.get(0) );
+        initChapter( introChapters.get(0) );*/
 
 
-        /*FirebaseDatabaseHandler firebaseDatabaseHandler = new FirebaseDatabaseHandler(IntroActivity.this);
+        FirebaseDatabaseHandler firebaseDatabaseHandler = new FirebaseDatabaseHandler(IntroActivity.this);
         firebaseDatabaseHandler.getIntroChapters(new FirebaseDatabaseHandler.GetIntroChaptersListener() {
             @Override
             public void onSuccess(ArrayList<IntroChapter> introChapters) {
@@ -249,10 +320,10 @@ public class IntroActivity extends AppCompatActivity
             public void onError(DatabaseError error) {
                 CommonUtils.displaySnackBar(IntroActivity.this, R.string.unable_to_fetch_data);
             }
-        });*/
-        /*for( IntroChapter chapter : introChapters ) {
+        });
+        for( IntroChapter chapter : introChapters ) {
             firebaseDatabaseHandler.writeIntroChapter( chapter );
-        }*/
+        }
 
     }
     @Override
