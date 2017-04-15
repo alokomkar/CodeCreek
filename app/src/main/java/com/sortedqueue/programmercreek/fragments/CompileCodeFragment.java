81306e66a589fe8d841dd136b50a2232438437a8
alokomkar <alokomkar.gudikote@gmail.com>
2017-04-15 22:14:49 +0530

    Added import from file

--- app/src/main/java/com/sortedqueue/programmercreek/fragments/CompileCodeFragment.java
+++ app/src/main/java/com/sortedqueue/programmercreek/fragments/CompileCodeFragment.java
@@ -1,9 +1,14 @@
 package com.sortedqueue.programmercreek.fragments;
 
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.NestedScrollView;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -16,6 +21,7 @@ import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.sortedqueue.programmercreek.R;
 import com.sortedqueue.programmercreek.adapter.CodeEditorRecyclerAdapter;
@@ -26,7 +32,15 @@ import com.sortedqueue.programmercreek.database.firebase.IdResponse;
 import com.sortedqueue.programmercreek.interfaces.retrofit.SubmitCodeService;
 import com.sortedqueue.programmercreek.network.RetrofitCreator;
 import com.sortedqueue.programmercreek.util.AuxilaryUtils;
+import com.sortedqueue.programmercreek.util.FileUtils;
+import com.sortedqueue.programmercreek.util.PermissionUtils;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -40,7 +54,7 @@ import retrofit2.Response;
  * Created by Alok on 04/04/17.
  */
 
-public class CompileCodeFragment extends Fragment {
+public class CompileCodeFragment extends Fragment implements View.OnClickListener {
 
     @Bind(R.id.outputTextView)
     TextView outputTextView;
@@ -54,6 +68,10 @@ public class CompileCodeFragment extends Fragment {
     NestedScrollView outputScrollView;
     @Bind(R.id.codeEditRecyclerView)
     RecyclerView codeEditRecyclerView;
+    @Bind(R.id.languageTextView)
+    TextView languageTextView;
+    @Bind(R.id.importFromFileTextView)
+    TextView importFromFileTextView;
     private SubmitCodeService submitCodeService;
     private String TAG = CompileCodeFragment.class.getSimpleName();
     private Code code;
@@ -66,9 +84,8 @@ public class CompileCodeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_compile_code, container, false);
         ButterKnife.bind(this, view);
         compilerProgressLayout.setVisibility(View.GONE);
-
-
-
+        languageTextView.setOnClickListener(this);
+        importFromFileTextView.setOnClickListener(this);
         setupRecyclerView();
         submitCodeService = RetrofitCreator.createService(SubmitCodeService.class);
         return view;
@@ -115,7 +132,7 @@ public class CompileCodeFragment extends Fragment {
         codeMap.put("language", LanguageConstants.C_INDEX);
         String sourceCode = codeEditorRecyclerAdapter.getCode();
         codeMap.put("sourceCode", sourceCode);
-        if( code.getInput() != null ) {
+        if (code.getInput() != null) {
             codeMap.put("input", code.getInput());
         }
         outputTextView.setText("");
@@ -171,5 +188,77 @@ public class CompileCodeFragment extends Fragment {
 
     public void setParameter(Code code) {
         this.code = code;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch ( view.getId() ) {
+            case R.id.languageTextView :
+                break;
+            case R.id.importFromFileTextView :
+                importFromFile();
+                break;
+        }
+    }
+
+    private int REQUEST_CODE_SEARCH = 1000;
+    private void importFromFile() {
+        if( PermissionUtils.checkSelfPermission(this,
+                new String[]{
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE})) {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("text/plain");
+            startActivityForResult(Intent.createChooser(intent,
+                    "Load file from directory"), REQUEST_CODE_SEARCH);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PermissionUtils.PERMISSION_REQUEST) {
+            if (PermissionUtils.checkDeniedPermissions((AppCompatActivity) getActivity(), permissions).length == 0) {
+                importFromFile();
+            } else {
+                if (permissions.length == 3) {
+                    Toast.makeText(getContext(), "Some permissions were denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if( requestCode == REQUEST_CODE_SEARCH && resultCode == AppCompatActivity.RESULT_OK ) {
+            try {
+                Uri uri = data.getData();
+                if( uri != null ) {
+
+                    Log.d(TAG, "File Uri : " +  uri.getEncodedPath() + " Path "+ uri.getPath());
+                    String filepath = FileUtils.getPath(getContext(), uri);
+                    Log.d(TAG, "File path : " + filepath);
+                    InputStream fis = new FileInputStream(filepath);
+                    InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+                    BufferedReader br = new BufferedReader(isr);
+                    String line;
+                    String completeLine = "";
+                    while ((line = br.readLine()) != null) {
+                        completeLine += line.trim() + " ";
+                    }
+                }
+                else {
+                }
+                // Rest of code that converts txt file's content into arraylist
+            } catch (IOException e) {
+                e.printStackTrace();
+                // Codes that handles IOException
+            }
+        }
+        else
+            super.onActivityResult(requestCode, resultCode, data);
     }
 }
